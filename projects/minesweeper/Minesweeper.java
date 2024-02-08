f0a0cf70d00fc4efcf6c6a241829da3dc4e2694a
Peter Seibel <peter@gigamonkeys.com>
2024-02-08 09:02:06 -0800

    Counting bombs.

--- projects/minesweeper/Minesweeper.java
+++ projects/minesweeper/Minesweeper.java
@@ -4,11 +4,14 @@ import java.awt.Graphics2D;
 public class Minesweeper extends GridGame {
 
   private boolean[][] bombs;
+  private int[][] counts;
 
   public Minesweeper(int rows, int columns) {
     super(rows, columns, 10);
     bombs = new boolean[rows][columns];
+    counts = new int[rows][columns];
     placeBombs((int)(rows * columns * 0.2));
+    countAllNeighbors();
   }
 
   public void paintCell(int row, int column, Graphics2D g) {
@@ -18,6 +21,11 @@ public class Minesweeper extends GridGame {
       g.setColor(Color.GRAY);
     }
     g.fillRect(0, 0, cellWidth(), cellHeight());
+    if (!bombs[row][column]) {
+      g.setColor(Color.BLUE);
+      g.setFont(g.getFont().deriveFont((float)(cellHeight() * 3 / 4)));
+      g.drawString("" + counts[row][column], cellWidth() / 3, cellHeight() * 3 / 4);
+    }
   }
 
   public void cellClicked(int row, int col) {
@@ -46,5 +54,35 @@ public class Minesweeper extends GridGame {
       }
     }
     bombs[row][col] = true;
+  }
+
+  private void countAllNeighbors() {
+    for (int r = 0; r < getRows(); r++) {
+      for (int c = 0; c < getColumns(); c++) {
+        counts[r][c] = countNeighbors(r, c);
+      }
+    }
+  }
+
+  private int countNeighbors(int r, int c) {
+    int count = 0;
+    for (int i = -1; i <= 1; i++) {
+      for (int j = -1; j <= 1; j++) {
+        if (i != 0 || j != 0) {
+          if (isBomb(r + i, c + j)) {
+            count++;
+          }
+        }
+      }
+    }
+    return count;
+  }
+
+  private boolean isBomb(int r, int c) {
+    return inBounds(r, c) && bombs[r][c];
+  }
+
+  private boolean inBounds(int r, int c) {
+    return 0 <= r && r < getRows() && 0 <= c && c < getColumns();
   }
 }
